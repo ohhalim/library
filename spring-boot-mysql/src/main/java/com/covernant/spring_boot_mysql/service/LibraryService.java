e96dc8b84c2f798bd2c7474fbe0d2e954c136400
ohhalim <ohhalim777@gmail.com>
2026-04-22 16:31:33 +0900

    feat: 책 생성 서비스 로직 추가 (작성자 검증 후 엔티티 저장)

--- spring-boot-mysql/src/main/java/com/covernant/spring_boot_mysql/service/LibraryService.java
+++ spring-boot-mysql/src/main/java/com/covernant/spring_boot_mysql/service/LibraryService.java
@@ -2,12 +2,14 @@ package com.covernant.spring_boot_mysql.service;
 
 import com.covernant.spring_boot_mysql.model.Author;
 import com.covernant.spring_boot_mysql.model.Book;
+import com.covernant.spring_boot_mysql.model.request.BookCreationRequest;
 import com.covernant.spring_boot_mysql.repository.AuthorRepository;
 import com.covernant.spring_boot_mysql.repository.BookRepository;
 import com.covernant.spring_boot_mysql.repository.LendRepository;
 import com.covernant.spring_boot_mysql.repository.MemberRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -40,5 +42,16 @@ public class LibraryService {
         throw new EntityNotFoundException(
                 "Can't find any under given isbn: " + isbn);
     }
-    
+    public Book createBook(BookCreationRequest book) { // 요청으로 들어온 authorID 저자 엔티티 조회
+        Optional<Author> author = authorRepository.findById(book.getAuthorid());
+        if (!author.isPresent()) {
+            throw new EntityNotFoundException(
+                    "Author not found");
+        }
+
+        Book bookToCreate = new Book(); // 요청 DTO를 실제 저장할 Book 엔티티로 변환
+        BeanUtils.copyProperties(book, bookToCreate); //  DTO와 엔티티에서 이름이 같은 필드 name, isbn을 복사
+        bookToCreate.setAuthor(author.get());// dto에는 authorId만 있으므로 조회한 author 엔티티를 직접 연결, 연관관계를 완성
+        return bookRepository.save(bookToCreate); // 완성된 Book 엔티티를 DB에 저장하고 저장결과를 반환
+    }
 }
